3ad144206aeeb96859f556adbb919125f407dfc0
abdoulnasserhanki-rgb <abdoulnasserhanki@gmail.com>
2026-05-20 17:01:13 +0100

    Fix db allowed hosts

--- scout/settings.py
+++ scout/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +31,8 @@ ALLOWED_HOSTS = [
 ]
 
 BASE_URL = 'http://localhost:8000'
+
+DATABASE_URL='postgresql://mon_projet_scout_db_user:IHIfQPuHe42C7TG2eXJtVLBddKfMb6VL@dpg-d86tcv37uimc73alot10-a/mon_projet_scout_db'
 
 AUTH_USER_MODEL = 'aderant.CustomUser'
 
@@ -113,10 +116,9 @@ WSGI_APPLICATION = 'scout.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
